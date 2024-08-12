b7b31c34ca6d940e9bb12e2eed37dc826e735b22
Merna Hesham <mernahesham21010@gmail.com>
2024-08-12 21:52:00 +0300

    Added Hello World Program

---
+++ A.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Merna Hesham
+ */
+/*A. Say Hello With C++
+time limit per test1 second
+memory limit per test256 megabytes
+Given a name S. Print "Hello, (name)" without parentheses.
+
+Input
+Only one line containing a string S.
+
+Output
+Print "Hello, " without quotes, then print name.
+
+Example
+InputCopy
+programmer
+OutputCopy
+Hello, programmer
+*/
+public class A {
+
+    public static void main(String[] args) {
+        for(int i = 0; i < 3; i++){
+        	// Print "Hello, " followed by the input string S
+        	System.out.println("Hello World, " + i);
+	}
+
+    }
+}
